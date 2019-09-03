e50a6c8f92d50c3b2a91f50fcf9d986e397caaf3
whatwewant <tobewhatwewant@gmail.com>
2019-09-03 18:44:59 +0800

    :sparkles:(packages.channel-koa): add koa channel(async/await)

---
+++ packages/channel-koa/src/client.ts
@@ -0,0 +1,42 @@
+import {
+  IRPCChannelClientSide,
+  MessageCallback,
+  Message,
+  EncodedMessage,
+} from '@zorpc/core';
+
+import { IClientConfig } from './interface';
+
+export class Client implements IRPCChannelClientSide<IClientConfig> {
+  private engine: (config: IClientConfig, message: EncodedMessage<any>) => Promise<EncodedMessage<any>>;
+  private callback: MessageCallback<any>;
+
+  constructor(public readonly config: IClientConfig) {}
+
+  public postMessage<I>(clientMessage: Message<I>): void {
+    const { appKey } = this.config;
+    const message = {
+      appKey,
+      ...clientMessage,
+    };
+
+    this.engine(this.config, message)
+      .then(serverMessage => {
+        this.callback(null, serverMessage);
+      }).catch(error => {
+        this.callback(error, clientMessage);
+      });;
+  }
+
+  public onMessage<Output>(callback: MessageCallback<Output>) {
+    this.callback = (error, message) => {
+      callback(error, message);
+    };
+  }
+
+  public useEngine(callback: (config: IClientConfig, message: EncodedMessage<any>) => Promise<EncodedMessage<any>>): void {
+    this.engine = (config, message) => {
+      return callback(config, message);
+    }
+  }
+}
---
+++ packages/channel-koa/src/server.ts
@@ -0,0 +1,79 @@
+import {
+  IRPCChannelServerSide,
+  MessageCallback,
+  Message,
+} from '@zorpc/core';
+import { Middleware, Context } from 'koa';
+import { IServerConfig, PassMessage } from './interface';
+
+export class Server implements IRPCChannelServerSide<IServerConfig> {
+  private server = {
+    consumers: {},
+    postMessage(output: Message<any>) {
+      this.useCallback(output);
+    },
+    onmessage(message: Message<any>): void {
+      throw new Error(`Server Override onMessage`);
+    },
+    createCallback<Output>(id: string, consumer: (output: Output) => void, done: Function) {
+      this.consumers[id] = (output: Output) => {
+        consumer(output);
+        // consume only once
+        delete this.consumers[id];
+      };
+
+      done();
+    },
+    useCallback(message: Message<any>) {
+      const consumeOnlyOnceCallback = this.consumers[message.id];
+
+      if (!consumeOnlyOnceCallback) {
+        throw new Error(`CallbackFor(${message.id}) doesnot exist. Maybe it have been consumed ?`)
+      }
+
+      consumeOnlyOnceCallback(JSON.stringify(message));
+    },
+  };
+
+  constructor(public readonly config: IServerConfig) {}
+
+  public postMessage<Output>(output: Message<Output>): void {
+    // @LOGGER need a logger: send message to client
+    this.server.postMessage(output);
+  }
+
+  public onMessage<Input>(callback: MessageCallback<Input>) {
+    // override onMessage
+    this.server.onmessage = (input: PassMessage<any>) => {
+      // @LOGGER need a logger: receive message from client
+      callback(null, input);
+    };
+  }
+
+  public middleware(): Middleware {
+    const { method, path } = this.config;
+    return async (ctx: Context, next: Function) => {
+      // @TODO here, method only support POST
+      if (ctx.method === method && ctx.path === path) {
+        // @LOGGER need a logger: hit rpc
+        const input = (ctx.request as any).body;
+        // @TODO should valid message id before use input.id
+        return await new Promise((resolve, reject) => {
+          this.server.createCallback(
+            input.id,
+            (output: string) => {
+              ctx.status = 200;
+              ctx.body = JSON.parse(output);
+              resolve();
+            },
+            () => {
+              this.server.onmessage(input);
+            },
+          );
+        });
+      }
+
+      await next();
+    }
+  }
+}
